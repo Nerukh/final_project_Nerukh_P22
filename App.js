fde6dccf583048b089c5827968937d5a13d2f81f
Nerukh <nv1923730@gmail.com>
2025-12-19 00:20:20 +0200

    Install BottomTabNavigation

--- App.js
+++ App.js
@@ -1,15 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+
+const CalendarScreen = () => (
+    <View style={styles.container}>
+        <Text>Calendar Screen</Text>
+    </View>
+);
+
+const MapScreen = () => (
+    <View style={styles.container}>
+        <Text>Map Screen</Text>
+    </View>
+);
+
+const NewScreen = () => (
+    <View style={styles.container}>
+        <Text>New Screen</Text>
+    </View>
+);
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Tab.Navigator screenOptions={{ headerShown: false }}>
+                <Tab.Screen name="Calendar" component={CalendarScreen} />
+                <Tab.Screen name="Map" component={MapScreen} />
+                <Tab.Screen name="New" component={NewScreen} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
